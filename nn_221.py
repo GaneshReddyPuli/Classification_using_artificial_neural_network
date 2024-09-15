71a68a2f6cf1a2865aaa5516b60f8e1e717b1468
Ganesh Reddy Puli <puliganeshreddy11@gmail.com>
2024-09-15 15:20:11 -0500

    first commit

---
+++ nn_221.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 15 14:07:19 2024
+
+@author: ganeshreddypuli
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+# A function to create a dataset.
+from sklearn.datasets import make_blobs
+
+# Set a seed so that the results are consistent.
+np.random.seed(3)
+
+m = 2000
+samples, labels = make_blobs(n_samples=m, 
+                             centers=([2.5, 3], [6.7, 7.9], [2.1, 7.9], [7.4, 2.8]), 
+                             cluster_std=1.1,
+                             random_state=0)
+labels[(labels == 0) | (labels == 1)] = 1
+labels[(labels == 2) | (labels == 3)] = 0
+X = np.transpose(samples)
+Y = labels.reshape((1, m))
+
+plt.figure()
+plt.scatter(X[0, :], X[1, :], c=Y, cmap=colors.ListedColormap(['blue', 'red']));
+plt.title("Original Dataset")
+#print(X)
+#print(Y)
+print("Dataset:")
+print ('The shape of X is: ' + str(X.shape))
+print ('The shape of Y is: ' + str(Y.shape))
+print ('No. of training examples =', m)
+
+def sigmoid(z):
+
+    res = 1 / (1 + np.exp(-z))
+    
+    return res
+
+def layer_sizes(X, Y):
+
+    n_x = X.shape[0]
+    n_h = 2
+    n_y = Y.shape[0] 
+
+    return (n_x, n_h, n_y)
+
+(n_x, n_h, n_y) = layer_sizes(X, Y)
+print("\nThe size of the input layer is: n_x = " + str(n_x))
+print("The size of the hidden layer is: n_h = " + str(n_h))
+print("The size of the output layer is: n_y = " + str(n_y))
+
+def initialize_parameters(n_x, n_h, n_y):
+
+    W1 = np.random.randn(n_h, n_x) * 0.01        
+    b1 = np.zeros((n_h, 1)) * 0.01
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1)) * 0.01
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def forward_propagation(X, parameters):
+
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    Z1 = np.matmul(W1,X) + b1
+    A1 = sigmoid(Z1)
+    Z2 = np.matmul(W2,A1) + b2
+    A2 = sigmoid(Z2)
+
+    assert(A2.shape == (n_y, X.shape[1]))
+
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+def compute_cost(A2, Y):
+
+    m = Y.shape[1]
+
+    logloss = np.sum(-Y * np.log(A2) - (1-Y) * np.log(1-A2))   # Element-wise log loss
+    cost = logloss/ m
+
+    assert(isinstance(cost, float))
+    
+    return cost
+
+def backward_propagation(parameters, cache, X, Y):
+
+    m = X.shape[1]
+
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    # Backward propagation: calculate partial derivatives denoted as dW1, db1, dW2, db2 for simplicity. 
+    dZ2 = A2 - Y
+    dW2 = 1/m * np.dot(dZ2, A1.T)
+    db2 = 1/m * np.sum(dZ2, axis = 1, keepdims = True)
+    dZ1 = np.dot(W2.T, dZ2) * A1 * (1 - A1)
+    dW1 = 1/m * np.dot(dZ1, X.T)
+    db1 = 1/m * np.sum(dZ1, axis = 1, keepdims = True)
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
+
+def update_parameters(parameters, grads, learning_rate=1.2):
+
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def nn_model(X, Y, n_h, num_iterations=10, learning_rate=1.2, print_cost=False):
+
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+
+    parameters = initialize_parameters(n_x, n_h, n_y)
+
+    for i in range(0, num_iterations):
+
+        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+        A2, cache = forward_propagation(X, parameters)
+        
+        # Cost function. Inputs: "A2, Y". Outputs: "cost".
+        cost = compute_cost(A2, Y)
+        
+        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
+        grads = backward_propagation(parameters, cache, X, Y)
+        
+        # Gradient descent parameter update. Inputs: "parameters, grads, learning_rate". Outputs: "parameters".
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        # Print the cost every iteration.
+        #if print_cost:
+            #print ("Cost after iteration %i: %f" %(i, cost))
+
+    return parameters
+
+parameters = nn_model(X, Y, n_h=2, num_iterations=3000, learning_rate=1.2, print_cost=True)
+print("\nFinal parameters: ")
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+W1 = parameters["W1"]
+b1 = parameters["b1"]
+W2 = parameters["W2"]
+b2 = parameters["b2"]
+
+def predict(X, parameters):
+
+    A2, cache = forward_propagation(X, parameters)
+    predictions = A2 > 0.5
+
+    return A2, predictions
+
+X_sample_pred = np.array([[2, 8, 2, 8], [2, 8, 8, 2]])
+Y_sample_pred = predict(X_sample_pred, parameters)[1]
+
+print("\nSample testing:")
+print(f"Coordinates (in the columns):\n{X_sample_pred}")
+print(f"Predictions:\n{Y_sample_pred}")
+
+print("\nReal testing:")
+Y_hat,real_predictions = predict(X, parameters)
+cost = compute_cost(Y_hat, Y)
+print("Cost of our model:",cost)
+
+def plot_decision_boundary(predict, parameters, X, Y):
+    # Define bounds of the domain.
+    min1, max1 = X[0, :].min()-1, X[0, :].max()+1
+    min2, max2 = X[1, :].min()-1, X[1, :].max()+1
+    # Define the x and y scale.
+    x1grid = np.arange(min1, max1, 0.1)
+    x2grid = np.arange(min2, max2, 0.1)
+    # Create all of the lines and rows of the grid.
+    xx, yy = np.meshgrid(x1grid, x2grid)
+    # Flatten each grid to a vector.
+    r1, r2 = xx.flatten(), yy.flatten()
+    r1, r2 = r1.reshape((1, len(r1))), r2.reshape((1, len(r2)))
+    # Vertical stack vectors to create x1,x2 input for the model.
+    grid = np.vstack((r1,r2))
+    # Make predictions for the grid.
+    predictions = predict(grid, parameters)[1]
+    # Reshape the predictions back into a grid.
+    zz = predictions.reshape(xx.shape)
+    # Plot the grid of x, y and z values as a surface.
+    plt.contourf(xx, yy, zz, cmap=plt.cm.Spectral.reversed())
+    plt.contour(xx, yy, zz, levels=[0.5], colors='white')
+    plt.scatter(X[0, :], X[1, :], c=Y, cmap=colors.ListedColormap(['blue', 'red']));
+
+# Plot the decision boundary.
+plt.figure()
+plot_decision_boundary(predict, parameters, X, Y)
+plt.title("Decision Boundary for hidden layer size " + str(n_h))
+
+
+'''# Try it for other dataset
+n_samples = 2000
+samples, labels = make_blobs(n_samples=n_samples, 
+                             centers=([2.5, 3], [6.7, 7.9], [2.1, 7.9], [7.4, 2.8]), 
+                             cluster_std=1.1,
+                             random_state=0)
+labels[(labels == 0)] = 0
+labels[(labels == 1)] = 1
+labels[(labels == 2) | (labels == 3)] = 1
+X_2 = np.transpose(samples)
+Y_2 = labels.reshape((1,n_samples))
+
+plt.scatter(X_2[0, :], X_2[1, :], c=Y_2, cmap=colors.ListedColormap(['blue', 'red']));
+
+# parameters_2 = nn_model(X_2, Y_2, n_h=1, num_iterations=3000, learning_rate=1.2, print_cost=False)
+parameters_2 = nn_model(X_2, Y_2, n_h=2, num_iterations=3000, learning_rate=1.2, print_cost=False)
+# parameters_2 = nn_model(X_2, Y_2, n_h=15, num_iterations=3000, learning_rate=1.2, print_cost=False)
+
+# This function will call predict function 
+plt.figure()
+plot_decision_boundary(predict, parameters_2, X_2, Y_2)
+plt.title("Decision Boundary")'''
